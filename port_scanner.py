4e537be392f2fadeee34f4e09215991a92546869
dopehug0x <felipehpmachado@hotmail.com>
2026-03-21 18:01:32 -0300

    .
    .

--- port_scanner.py
+++ port_scanner.py
@@ -1,15 +1,17 @@
 import socket
 import time
+import random
+import concurrent.futures
 
 
 
-
-def scanning(host, port, timeout=2):
+def scanning(ip, port, timeout=2):
     
+
     try:
 
         client = None
-        ip = socket.gethostbyname(host)
+        
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.settimeout(timeout)
         response = client.connect_ex((ip, port))
@@ -19,20 +21,33 @@ def scanning(host, port, timeout=2):
             print(f'[+]Port {port} is closed/filtered from {host} ')
     except KeyboardInterrupt:
         print(f'[+]Scanner interrupted.')
-    except socket.gaierror: #DNS error
-        print(f'[+]Hostname {host} doesnt exists.')
     except Exception as e:
         print(f'[+]Error: {e} is occurred')
     finally:
         if client:
             client.close()
 
-host = "www.googliurieiiaiaii.com"
-port = 22
+host = "www.google.com"
 
-scanning(host,port)
-#ports = list(range(1,1025))
+#Get IPV4 and DNS treatment.
+ip = None
+try:
+    ip = socket.gethostbyname(host)
 
+except socket.gaierror:
+    print(f'[+]Hostname {host} does not exists, fix it or check your DNS server')
+    exit()
+    
+
+port = 443
+
+#Setting workers to assync scanning
+with ThreadPoolExecutor(max-workers=100) as executor:
+    future = executor.submit
+
+scanning(ip,port)
+ports = list((range(1,1025)))
+random.shuffle(ports)
     
     
     
