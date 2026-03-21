7c29a1fa25360f7475521cc9e7b6362d083509f9
dopehug0x <felipehpmachado@hotmail.com>
2026-03-21 16:33:55 -0300

    evolving error treatment and ip variable in scanning function
    included keyboard interrupt exception, validate if clients is None to close socket, also removed return redundancy and included ip variable to establish the socket connection with IPV4, avoiding the dns traffic or dns errors.

--- port_scanner.py
+++ port_scanner.py
@@ -3,29 +3,34 @@ import time
 
 
 
-def scanning(link, port, time=2):
 
+def scanning(host, port, timeout=2):
+    
     try:
 
         client = None
+        ip = socket.gethostbyname(host)
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        response = client.connect_ex((link, port))
+        client.settimeout(timeout)
+        response = client.connect_ex((ip, port))
         if response == 0:
-            print(f"[+]Port {port} is open from {link}")
-            return None
+            print(f"[+]Port {port} is open from {host}")
         else:
-            print(f'[+]Port {port} is closed/filtered from {link} ')
-            return None
+            print(f'[+]Port {port} is closed/filtered from {host} ')
+    except KeyboardInterrupt:
+        print(f'[+]Scanner interrupted.')
+    except socket.gaierror: #DNS error
+        print(f'[+]Hostname {host} doesnt exists.')
     except Exception as e:
-        print(f'Error: {e} is occurred')
-        return None
+        print(f'[+]Error: {e} is occurred')
     finally:
-        client.close()
+        if client:
+            client.close()
 
-link = "www.google.com"
+host = "www.googliurieiiaiaii.com"
 port = 22
 
-scanning(link,port)
+scanning(host,port)
 #ports = list(range(1,1025))
 
     
